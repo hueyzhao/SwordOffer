18f32e85b356c188a4a4c26e09e9f9efdb477d6a
"huanyu" <zhaohuanyu415@gmail.com>
2015-05-19 14:48:28 +0800

    debug done

--- src/Problem27/ConvertTree2DoubleLink.java
+++ src/Problem27/ConvertTree2DoubleLink.java
@@ -1,19 +1,20 @@
 package Problem27;
 
 import Common.BinaryTree.BinaryTreeNode;
+import Problem6.ConstructBinaryTree;
 
 public class ConvertTree2DoubleLink<T> {
 
 		  private boolean first = true;
           private BinaryTreeNode<T> head; 
-
-          public void convertTree(BinaryTreeNode<T> root, BinaryTreeNode<T> lastInList){
+          private BinaryTreeNode<T> lastInList;
+          private void convertTree(BinaryTreeNode<T> root){
                if(root == null)
                     return;
           if(root.getLeftChild()!=null)
-               convertTree(root.getLeftChild(), lastInList);
+               convertTree(root.getLeftChild());
           BinaryTreeNode<T> current = root;
-          if(lastInList!=null){
+          if(this.lastInList!=null){
                current.setLeftChild(lastInList);
                lastInList.setRightChild(current);
           }
@@ -23,11 +24,23 @@ public class ConvertTree2DoubleLink<T> {
           }
           lastInList = current;
           if(root.getRightChild()!=null)
-               convertTree(root.getRightChild(),lastInList);
+               convertTree(root.getRightChild());
      }
 
       public BinaryTreeNode<T> convertTreetoDoubleList(BinaryTreeNode<T> root){
-          convertTree(root, null);
+          convertTree(root);
           return this.head;
      }
+      public static void main(String [] args){
+    	  String preOrder = "123456789";
+			String inOrder = "123456789";
+			ConstructBinaryTree test = new ConstructBinaryTree();
+			BinaryTreeNode<Character> tree = test.constructTree(preOrder,inOrder);
+			ConvertTree2DoubleLink<Character> convertTree = new ConvertTree2DoubleLink<Character>();
+			BinaryTreeNode<Character> head = convertTree.convertTreetoDoubleList(tree);
+			while(head!=null){
+				System.out.print(head.getData() + " ");
+				head = head.getRightChild();
+			}
+      }
 }
