eabce213ef9d41c71e3c3e3b9e56bb8373121486
huanyu <zhaohuanyu415@gmail.com>
2015-05-31 13:10:04 +0800

    greatest sub array

---
+++ src/Problem31/GreatestSubArray.java
@@ -0,0 +1,30 @@
+package Problem31;
+
+public class GreatestSubArray {
+	public static void main(String [] args){
+		GreatestSubArray subArray = new GreatestSubArray();
+		int[] array = {1,2,4};
+		System.out.println(subArray.getGreatestSum(array));
+	}
+
+public int getGreatestSum(int [] array){
+          if(array == null || array.length <=0){
+          System.out.println("Invalid input");
+          return -1;
+          }
+          int maxSum = array[0];
+          int tempSum = 0;
+          for(int i =0; i< array.length; i++){
+               tempSum = tempSum + array[i];
+               if(tempSum<array[i]){
+                    tempSum = array[i];
+               }
+               if(tempSum > maxSum)
+                    maxSum = tempSum;
+               
+          }
+          return maxSum;
+     
+     }
+
+}
