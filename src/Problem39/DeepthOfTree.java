79067cc476164f38eda9053f69ce4b12c51759da
huanyu <zhaohuanyu415@gmail.com>
2015-06-24 07:35:46 +0800

    Deepth

--- src/Problem39/DeepthOfTree.java
+++ src/Problem39/DeepthOfTree.java
@@ -1,24 +1,25 @@
 package Problem39;
 
 import Common.BinaryTree.BinaryTreeNode;
+import Problem6.ConstructBinaryTree;
 
 public class DeepthOfTree<T> {
-	private int treeDepth = 0;
-	private int maxDepth = 0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String preOrder = "12345";
+		String inOrder = "12345";
+		ConstructBinaryTree test = new ConstructBinaryTree();
+		BinaryTreeNode<Character> tree = test.constructTree(preOrder,inOrder);
+		DeepthOfTree<Character> testTree = new DeepthOfTree<Character>();
+		System.out.println(testTree.getDepth(tree));
 	}
 	public int getDepth(BinaryTreeNode<T> binaryTree){
 		if(binaryTree == null)
-			return -1;
+			return 0;
 		
-		if(binaryTree.getLeftChild()!=null) maxDepth++;
-		if(maxDepth>treeDepth) treeDepth = maxDepth;
-		getDepth(binaryTree.getLeftChild());
-		if(binaryTree.getRightChild()!=null) maxDepth++;
-		getDepth(binaryTree.getRightChild());
-		return treeDepth;
+		int leftDepth = getDepth(binaryTree.getLeftChild());
+		int rightDepth = getDepth(binaryTree.getRightChild());
+		return (leftDepth > rightDepth)?(leftDepth + 1):(rightDepth + 1);
 	}
 
 }
