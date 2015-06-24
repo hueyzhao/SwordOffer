94acee0f3e01ddfb1fb75634dfc42c6b5411bb53
huanyu <zhaohuanyu415@gmail.com>
2015-06-24 08:25:46 +0800

    Balance Tree

---
+++ src/Problem39/BalanceResult.java
@@ -0,0 +1,19 @@
+package Problem39;
+
+public class BalanceResult {
+	private boolean isBalanced;
+	public boolean isBalanced() {
+		return isBalanced;
+	}
+	public void setBalanced(boolean isBalanced) {
+		this.isBalanced = isBalanced;
+	}
+	public int getDeepth() {
+		return deepth;
+	}
+	public void setDeepth(int deepth) {
+		this.deepth = deepth;
+	}
+	private int deepth;
+	
+}
--- src/Problem39/DeepthOfTree.java
+++ src/Problem39/DeepthOfTree.java
@@ -4,6 +4,7 @@ import Common.BinaryTree.BinaryTreeNode;
 import Problem6.ConstructBinaryTree;
 
 public class DeepthOfTree<T> {
+	private int balanceDeepth = 0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String preOrder = "12345";
@@ -20,6 +21,48 @@ public class DeepthOfTree<T> {
 		int leftDepth = getDepth(binaryTree.getLeftChild());
 		int rightDepth = getDepth(binaryTree.getRightChild());
 		return (leftDepth > rightDepth)?(leftDepth + 1):(rightDepth + 1);
+	}
+	/**
+	 * post order 
+	 * @param root
+	 * @return
+	 */
+	public boolean isBalanceTree(BinaryTreeNode<T> root){
+		if(root == null){
+			return true;
+		}
+		BalanceResult isBalanced = isBalanced(root);
+		return isBalanced.isBalanced();
+		
+	}
+	
+	private BalanceResult isBalanced(BinaryTreeNode<T> root){
+		if(root == null){
+			return null;
+		}
+		int left = 0;
+		int right = 0;
+		int deepth = 0;
+		BalanceResult leftResult = isBalanced(root.getLeftChild());
+		BalanceResult rightResult = isBalanced(root.getRightChild());
+		left = leftResult.getDeepth();
+		right = rightResult.getDeepth();
+		if(leftResult.isBalanced()&&rightResult.isBalanced()){
+			
+			int diff = left - right;
+			if(diff <=1 || diff >= -1){
+				BalanceResult balance = new BalanceResult();
+				balance.setBalanced(true);
+				deepth = (left>right) ? (left+1) : (right+1);
+				balance.setDeepth(deepth);
+				return balance;
+			}
+		}
+		BalanceResult falseResult = new BalanceResult();
+		falseResult.setBalanced(false);
+		deepth = (left>right) ? (left+1) : (right+1);
+		falseResult.setDeepth(deepth);
+		return falseResult;
 	}
 
 }
