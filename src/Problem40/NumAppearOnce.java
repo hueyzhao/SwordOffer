4eedf70bdeb4e830fb57c874b3423a1afbcf2772
huanyu <zhaohuanyu415@gmail.com>
2015-06-28 22:44:24 +0800

    first commit

---
+++ src/Problem40/NumAppearOnce.java
@@ -0,0 +1,60 @@
+package Problem40;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumAppearOnce {
+	public List<Integer> findNumberAppearOnce(int [] intArray){
+		List<Integer> resultList = new ArrayList<Integer>();
+		if(intArray == null || intArray.length<0)
+			return null;
+		if(intArray.length == 1){
+			resultList.add(intArray[0]);
+			return resultList;
+		}
+			
+		int arrayXOR = intArray[0];
+		for(int i = 1; i < intArray.length; i++){
+			arrayXOR ^= intArray[i];
+		}
+		int firstBitNum = firstBit1(arrayXOR);
+		int firstNum = 0;
+		int secNum = 0;
+		for(int i = 0; i < intArray.length; i++){
+			if((firstBitNum&intArray[i])==0){
+				if(firstNum == 0)
+					firstNum = intArray[i];
+				else
+					firstNum ^= intArray[i];
+			}
+			else{
+				if(secNum == 0)
+					secNum = intArray[i];
+				else
+					secNum ^= intArray[i];
+			}
+			resultList.add(firstNum);
+			resultList.add(secNum);
+		}
+		return resultList;
+		
+	}
+	
+	private int firstBit1(int number){
+		if(number==0)
+			return -1;
+		int bitIndex = 0;
+		while(number==0){
+			number = number >> 1;
+			bitIndex ++;
+		}
+		int result = 1;
+		while(bitIndex > 0){
+			result = result <<1;
+			--bitIndex;
+		}
+		return result;
+	}
+	
+	
+}
---
+++ src/Problem40/NumAppearOnceTest.java
@@ -0,0 +1,24 @@
+package Problem40;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class NumAppearOnceTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFindNumberAppearOnce() {
+		fail("Not yet implemented");
+	}
+
+}
