e7296994d4633689937bf233e509042d0987dd31
huanyu <zhaohuanyu415@gmail.com>
2015-06-30 00:03:10 +0800

    First Complete

--- src/Problem41/FindSumNums.java
+++ src/Problem41/FindSumNums.java
@@ -7,34 +7,29 @@ public class FindSumNums {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int [] dataArray = {1,2,4,7,11,15};
+		FindSumNums sumNums = new FindSumNums();
+		sumNums.printSumNums(dataArray, 15);
 	}
 	public void printSumNums(int [] dataArray, int sum){
 		if(dataArray == null || dataArray.length <= 0)
 			return ;
 		int first = 0;
 		int last = dataArray.length -1;
-		int tempSum = dataArray[first] + dataArray[last];
-		List<Integer> sumArray = new ArrayList<Integer>();
-		sumArray.add(dataArray[first]);
-		sumArray.add(dataArray[last]);
+		
+		
 		while(first!=last){
+			int tempSum = dataArray[first] + dataArray[last];
 			if(tempSum == sum){
-				for(Integer item: sumArray)
-					System.out.println(item + " ");
-				System.out.println();
+				System.out.println(dataArray[first]);
+				System.out.println(dataArray[last]);
+				break;
 			}
 			if(tempSum < sum){
 				first ++;
-				tempSum += dataArray[first];
-				sumArray.add(dataArray[first]);
 			}
 			if(tempSum > sum){
-				sumArray.remove(sumArray.indexOf(dataArray[last]));
-				tempSum -= dataArray[last];
 				last--;
-				tempSum += dataArray[last];
-				sumArray.add(dataArray[last]);
 				
 			}
 		}
