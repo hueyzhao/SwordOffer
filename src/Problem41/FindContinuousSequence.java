330b98da166c776992671025159d1a88cbb132f4
huanyu <zhaohuanyu415@gmail.com>
2015-06-30 09:02:30 +0800

    second Solved

---
+++ src/Problem41/FindContinuousSequence.java
@@ -0,0 +1,40 @@
+package Problem41;
+
+public class FindContinuousSequence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public void printContinuousSequence(int num){
+		if(num <= 2)
+			return ;
+		int startNum = 1;
+		int endNum = 2;
+		int tempSum = startNum + endNum;
+		
+		while(endNum < (num+1)/2){
+			if(tempSum < num){
+				endNum++;
+				tempSum += endNum;
+			}
+			else if(tempSum > num){
+				tempSum -= startNum;
+				startNum++;
+				tempSum += startNum;
+			}
+			else{
+				print2End(startNum, endNum);
+			}
+				
+		}
+	}
+	private void print2End(int startNum, int endNum){
+		if(startNum<0||endNum<0||startNum>endNum)
+			return ;
+		for(int num = startNum; num<=endNum; num++)
+			System.out.print(num + " ");
+		System.out.println();
+	}
+
+}
