5f93a741b00e1206878128163fdf28c37f2fe384
"huanyu" <zhaohuanyu415@gmail.com>
2015-07-07 13:47:24 +0800

    init

---
+++ src/Problem49/String2Int.java
@@ -0,0 +1,10 @@
+package Problem49;
+
+public class String2Int {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Problem50/CommonAncestor.java
@@ -0,0 +1,10 @@
+package Problem50;
+
+public class CommonAncestor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
