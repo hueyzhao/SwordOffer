05e002c102f297f66022b07956b35daad8259138
"huanyu" <zhaohuanyu415@gmail.com>
2015-07-08 14:04:33 +0800

    wrong index

--- src/Problem9/Fibonacci.java
+++ src/Problem9/Fibonacci.java
@@ -4,7 +4,7 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(fibonacci(10000000));//still need improve, big data can't be processed
+		System.out.println(fibonacci(2));//still need improve, big data can't be processed
 
 	}
 	public static long fibonacci(long n){
@@ -13,9 +13,9 @@ public class Fibonacci {
         int result[] = {0,1};
          if(n<2)
               return result[(int)n];
-         long index =3;
-         long  first = 1;
-         long second = 2;
+         long index =2;
+         long  first = 0;
+         long second = 1;
          long fib = 0;
          while(index<=n){
          fib = first + second;
