b783494fd2dc92d5d8eee57452ce091851c3b0ec
"huanyu" <zhaohuanyu415@gmail.com>
2015-07-08 16:01:34 +0800

    index method

--- src/Problem6/ConstructBinaryTree.java
+++ src/Problem6/ConstructBinaryTree.java
@@ -9,7 +9,7 @@ public class ConstructBinaryTree {
 		String preOrder = "12473568";
 		String inOrder = "47215386";
 		ConstructBinaryTree test = new ConstructBinaryTree();
-		BinaryTreeNode<Character> tree = test.constructTree(preOrder,inOrder);
+		BinaryTreeNode<Character> tree = test.constructCore(preOrder,inOrder, 0, preOrder.length()-1,0,inOrder.length()-1);
 		test.preOrder(tree);
 	}
 	public BinaryTreeNode<Character> constructTree(String preOrder,String inOrder){
@@ -54,6 +54,51 @@ public class ConstructBinaryTree {
 			if(rootNode.getRightChild() != null)
 				preOrder(rootNode.getRightChild());
 		}
+	}
+	
+	private BinaryTreeNode<Character> constructCore(String preOrder, String inOrder, int preOrderStart, int preOrderEnd, int inOrderStart, int inOrderEnd){
+		
+		if(preOrder == null || inOrder == null)
+			return null;
+		if(preOrder.length()!=inOrder.length())
+			return null;
+		if(preOrderStart < 0 || preOrderStart > preOrder.length() || inOrderStart < 0 || inOrderEnd > inOrder.length())
+			return null;
+		if(preOrderStart > preOrderEnd || inOrderStart > inOrderEnd)
+			return null;
+		int preLength = preOrderEnd - preOrderStart;
+		int inOrderLength = inOrderEnd - inOrderStart;
+		if(preLength != inOrderLength)
+			return null;
+		char [] preOrderArray = preOrder.toCharArray();
+		char [] inOrderArray = inOrder.toCharArray();
+		char rootChar = preOrderArray[preOrderStart];
+		int rootIndex = returnCharIndex(inOrderArray, rootChar, inOrderStart, inOrderEnd);
+		int leftLength = rootIndex - inOrderStart;
+		int rightLenght = inOrderEnd - rootIndex;
+		if(rootIndex == -1)
+			return null;
+		BinaryTreeNode<Character> root = new BinaryTreeNode<Character>(rootChar, null, null);
+		if(preLength >0)
+			root.setLeftChild(constructCore(preOrder,inOrder,preOrderStart+1, preOrderStart + leftLength,inOrderStart,rootIndex-1));
+		if(inOrderLength > 0)
+			root.setRightChild(constructCore(preOrder, inOrder, preOrderStart + leftLength + 1, preOrderEnd, rootIndex+1, inOrderEnd));
+		return root;
+	}
+	
+	private int returnCharIndex(char [] array, char data, int startIndex, int endIndex){
+		if(array == null)
+			return -1;
+		if(startIndex<0 || startIndex > array.length || endIndex < 0 || endIndex > array.length)
+			return -1;
+		if(startIndex > endIndex)
+			return -1;
+		int index = -1;
+		for(int i =startIndex; i <=endIndex; i++){
+			if(array[i] == data)
+				index = i;
+		}
+		return index;
 	}
 
 }
